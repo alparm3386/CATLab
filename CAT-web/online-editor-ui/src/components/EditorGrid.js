5e33c103edbb088ad550c0c95de1e6b3a76d67bf
Alpar Meszaros <alpar.meszaros@toppandigital.com>
2023-07-20 21:20:17 +0200

    redux

--- CAT-web/online-editor-ui/src/components/EditorGrid.js
+++ CAT-web/online-editor-ui/src/components/EditorGrid.js
@@ -3,14 +3,12 @@ import StatusBarContext from './../contexts/StatusBarContext';
 
 var renderCntr = 0;
 const EditorGrid = React.memo(function EditorGrid () {
-    //const { message } = useContext(StatusBarContext);
 
     console.log("EditorGrid rendered: " + renderCntr++);
 
     return (
         <div className="grid-area">
             grid
-            {/*message*/} {/* Grid content goes here */}
         </div>
     );
 });
