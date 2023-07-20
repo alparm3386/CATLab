b7346b4750cc465902d4e7211a26961fba1f65f0
Alpar Meszaros <alpar.meszaros@toppandigital.com>
2023-07-20 23:46:11 +0200

    =

--- CAT-web/online-editor-ui/src/App.js
+++ CAT-web/online-editor-ui/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect } from 'react';
 import 'bootstrap/dist/css/bootstrap.css'; // If you haven't linked Bootstrap in your HTML
 import './App.scss';
 import ContentArea from './components/ContentArea';
@@ -6,8 +6,20 @@ import Navbar from './components/Navbar';
 import StatusBar from './components/StatusBar';
 import { Provider } from 'react-redux';
 import store from './store/store';
+import { loadEditorData } from './api/apiService';
 
 function App() {
+    let ignore = false;
+    useEffect(() => {
+        if (!ignore) {
+            console.log("App start ...")
+            //load the data and store it in the global store
+            let editorData = loadEditorData();
+        }
+
+        return () => { ignore = true; }
+    }, []);
+
     return (
         <Provider store={store}>
             <div className="app">
