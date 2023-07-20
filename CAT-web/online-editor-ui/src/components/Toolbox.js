e170c908f3452482f0258561f8dcd923f4a332ee
Alpar Meszaros <alpar.meszaros@toppandigital.com>
2023-07-20 13:27:55 +0200

    toolbox component

---
+++ CAT-web/online-editor-ui/src/components/Toolbox.js
@@ -0,0 +1,7 @@
+﻿import React from 'react';
+
+const Toolbox = ({ expanded }) => {
+    return <div className={`toolbox-area${expanded ? ' expanded' : ''}`}>tool box</div>;
+};
+
+export default Toolbox;
