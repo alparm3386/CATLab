c65972c85f7a27d4f12f3d3f8558f6ba9f79a88d
Alpar Meszaros <alpar.meszaros@toppandigital.com>
2023-07-21 13:01:58 +0200

    -

--- CAT-web/online-editor-ui/src/components/Toolbox.js
+++ CAT-web/online-editor-ui/src/components/Toolbox.js
@@ -1,37 +1,20 @@
 ﻿// Toolbox.js
 import './../styles/toolboxArea.scss';
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { setStatusBarMessage } from '../store/editorDataSlice';
-import { getTMMatches } from './../api/apiService';
+import TMMatches from './TMMatches';
 
 
 var renderCntr = 0;
 const Toolbox = ({ expanded, currentIdx }) => {
     const [counter, setCounter] = useState(1);
-    const [tmMatches, setTmMatches] = useState([]);
     const dispatch = useDispatch();
 
     console.log("Toolbox rendered: " + renderCntr++);
 
-    useEffect(() => {
-        const tmMatches = getTMMatches(currentIdx);
-        setTmMatches(tmMatches);
-        return () => {
-        };
-    }, [currentIdx]);
-
     return <div className={`toolbox-area${expanded ? ' expanded' : ''}`}>
-        <div>
-            {tmMatches && tmMatches.map((tmMatch, index) => (
-                <div key={index} className="tmm-row">
-                    <div className="tmm-row-num">{index + 1}</div>
-                    <div className="tmm-source">{tmMatch.source}</div>
-                    <div className="tmm-target">{tmMatch.target}</div>
-                    <div className="tmm-quality">{tmMatch.quality}</div>
-                </div>
-            ))}
-        </div>
+        <TMMatches />
         <button onClick={() => { dispatch(setStatusBarMessage(`New message from toolbox ${counter}`)); setCounter(counter + 1) }}>
             Click me to update the message
         </button>
