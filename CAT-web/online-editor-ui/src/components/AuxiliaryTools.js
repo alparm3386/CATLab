42dc477b8d0a84f74e88bfe8a8c92e674b7bf93c
Alpar Meszaros <alpar.meszaros@toppandigital.com>
2023-07-21 14:59:30 +0200

    -

--- CAT-web/online-editor-ui/src/components/AuxiliaryTools.js
+++ CAT-web/online-editor-ui/src/components/AuxiliaryTools.js
@@ -1,14 +1,40 @@
 ﻿// AuxiliaryTools.js
+import React, { useState } from 'react';
 import './../styles/auxiliaryTools.scss';
-
 
 var renderCntr = 0;
 const AuxiliaryTools = () => {
+    const [selectedTab, setSelectedTab] = useState('context');
     console.log("AuxiliaryTools rendered: " + renderCntr++);
 
-    return <div className="auxiliary-tools">
-        Auxiliary tools ...
-    </div>;
+    return (
+        <div className="auxiliary-tools">
+            <div className="tabs">
+                <button
+                    className={selectedTab === 'context' ? 'active' : ''}
+                    onClick={() => setSelectedTab('context')}
+                >
+                    Context
+                </button>
+                <button
+                    className={selectedTab === 'preview' ? 'active' : ''}
+                    onClick={() => setSelectedTab('preview')}
+                >
+                    Preview
+                </button>
+                <button
+                    className={selectedTab === 'concordance' ? 'active' : ''}
+                    onClick={() => setSelectedTab('concordance')}
+                >
+                    Concordance
+                </button>
+            </div>
+
+            {selectedTab === 'context' && <div>Context content ...</div>}
+            {selectedTab === 'preview' && <div>Preview content ...</div>}
+            {selectedTab === 'concordance' && <div>Concordance content ...</div>}
+        </div>
+    );
 };
 
 export default AuxiliaryTools;
