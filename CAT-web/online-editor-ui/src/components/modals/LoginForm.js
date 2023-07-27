7d47b5205cbed908f9379a2ffcdb1d15f6637407
Alpar Meszaros <alpar.meszaros@toppandigital.com>
2023-07-27 13:55:53 +0200

    -

--- CAT-web/online-editor-ui/src/components/modals/LoginForm.js
+++ CAT-web/online-editor-ui/src/components/modals/LoginForm.js
@@ -1,46 +1,64 @@
 ﻿import React, { useState } from 'react';
-import { Modal, Button, Form } from 'react-bootstrap'
-import { useSelector } from 'react-redux';
-import { useDispatch } from 'react-redux';
+import { Modal, Button, Form, Alert } from 'react-bootstrap';
+import { useSelector, useDispatch } from 'react-redux';
 import { login } from 'api/editorApi';
 import { setJWT } from 'store/editorDataSlice';
 
 export const LoginForm = () => {
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
+    const [error, setError] = useState(null);
+    const [rememberMe, setRememberMe] = useState(false);
     const isLoginModalOpen = useSelector((state) => state.appUi.login.isOpen);
     const dispatch = useDispatch();
 
-    const handleLogin = () => {
-        const getJWT = async () => {
-            try {
-                const result = await login(username, password);
-                dispatch(setJWT(result.data));
-            } catch (error) {
-                console.log(error);
+    // Get stored credentials from localStorage
+    React.useEffect(() => {
+        const storedCredentials = JSON.parse(localStorage.getItem("credentials"));
+        if (storedCredentials) {
+            setUsername(storedCredentials.username);
+            setPassword(storedCredentials.password);
+        }
+    }, []);
+
+    const handleLogin = async (event) => {
+        event.preventDefault(); // To prevent form submission
+
+        try {
+            const result = await login(username, password);
+            dispatch(setJWT(result.data));
+
+            if (rememberMe) {
+                localStorage.setItem("credentials", JSON.stringify({ username, password }));
+            } else {
+                localStorage.removeItem("credentials");
             }
-        };
 
-        // Call the async function
-        getJWT();
+            setUsername('');
+            setPassword('');
+            setError(null); // Clear the error state if login was successful
 
-        setUsername('');
-        setPassword('');
+        } catch (error) {
+            setError("Authentication Failed. Please try again."); // Set the error state if authentication fails
+            console.log(error);
+        }
     };
 
     return (
-        <Modal show={ isLoginModalOpen }>
-            <Modal.Header>
+        <Modal show={isLoginModalOpen}>
+            <Modal.Header closeButton>
                 <Modal.Title>Login</Modal.Title>
             </Modal.Header>
             <Modal.Body>
-                <div>
-                    <Form.Control className="mb-2" type="text" placeholder="Username" value={username} onChange={e => setUsername(e.target.value)} required/>
+                <Form onSubmit={handleLogin}>
+                    {error && <Alert variant="danger">{error}</Alert>}
+                    <Form.Control className="mb-2" type="text" placeholder="Username" value={username} onChange={e => setUsername(e.target.value)} required />
                     <Form.Control className="mb-2" type="password" placeholder="Password" value={password} onChange={e => setPassword(e.target.value)} required />
+                    <Form.Check type="checkbox" label="Remember me" checked={rememberMe} onChange={e => setRememberMe(e.target.checked)} />
                     <div className="d-flex justify-content-end mt-4">
-                        <Button onClick={handleLogin}>Login</Button>
+                        <Button type="submit">Login</Button>
                     </div>
-                </div>
+                </Form>
             </Modal.Body>
             <Modal.Footer>
             </Modal.Footer>
