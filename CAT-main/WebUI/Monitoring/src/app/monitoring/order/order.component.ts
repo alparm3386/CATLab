8af127f8940248585a2b61d4369c6704d3fcd947
Alpar Meszaros <alpar.meszaros@toppandigital.com>
2023-09-02 01:35:08 +0200

    -

--- CAT-main/WebUI/Monitoring/src/app/monitoring/order/order.component.ts
+++ CAT-main/WebUI/Monitoring/src/app/monitoring/order/order.component.ts
@@ -3,7 +3,7 @@ import { CommonModule } from '@angular/common';
 import { JobComponent } from './job/job.component';
 
 @Component({
-  selector: 'order',
+  selector: '[order]',
   standalone: true,
   imports: [CommonModule, JobComponent],
   templateUrl: './order.component.html',
