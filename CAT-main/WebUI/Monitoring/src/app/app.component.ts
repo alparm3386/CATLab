227a13850208e2852c08f57ab44fa5cb28aa0dc7
Alpar Meszaros <alpar.meszaros@toppandigital.com>
2023-09-01 15:58:11 +0200

    monitoring

--- CAT-main/WebUI/Monitoring/src/app/app.component.ts
+++ CAT-main/WebUI/Monitoring/src/app/app.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
+import { MonitoringComponent } from './monitoring/monitoring.component';
 
 @Component({
   selector: 'app-root',
+  standalone: true,
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  imports: [
+    MonitoringComponent,
+  ],
 })
 export class AppComponent {
   title = 'Monitoring';
