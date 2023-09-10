9484775ce429604765a4d9aa0ddb7663792a2a02
Alpar Meszaros <alpar.meszaros@toppandigital.com>
2023-09-10 10:11:48 +0200

    Roles

---
+++ CAT-main/Areas/Identity/Controllers/RolesController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CAT.Areas.Identity.Controllers
+{
+    [Authorize(Policy = "AdminOnly")]
+    public class RolesController : Controller
+    {
+        // GET: RolesController
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
