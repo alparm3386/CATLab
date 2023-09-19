847fc54e8e11b472944b85890cce88c3f4e1eade
Alpar Meszaros <alpar.meszaros@toppandigital.com>
2023-09-19 23:53:24 +0200

    -

---
+++ CAT-main/TagHelpers/GreetingTagHelper.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace CAT.TagHelpers
+{
+    [HtmlTargetElement("greeting")]
+    public class GreetingTagHelper : TagHelper
+    {
+        public string MyProperty { get; set; } = default!;
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.TagName = "div";  // Set the tag name
+            output.Content.SetContent($"Custom content with property: {MyProperty}");
+        }
+    }
+}
