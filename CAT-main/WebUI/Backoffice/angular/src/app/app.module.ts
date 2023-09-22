46729a8afe47bf701b0ca0140188e9af206ca265
Alpar Meszaros <alpar.meszaros@toppandigital.com>
2023-09-22 19:55:50 +0200

    monitoring

--- CAT-main/WebUI/Backoffice/angular/src/app/app.module.ts
+++ CAT-main/WebUI/Backoffice/angular/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MonitoringComponent } from './monitoring/monitoring.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MonitoringComponent
   ],
   imports: [
     BrowserModule,
