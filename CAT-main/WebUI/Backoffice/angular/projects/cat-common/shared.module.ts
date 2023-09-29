067cbbc724e5ded70fcae6690c3b4a7b0f902578
Alpar Meszaros <alpar.meszaros@toppandigital.com>
2023-09-29 13:57:37 +0200

    -

---
+++ CAT-main/WebUI/Backoffice/angular/projects/cat-common/shared.module.ts
@@ -0,0 +1,8 @@
+import { NgModule } from '@angular/core';
+import { NgxSpinnerService } from 'ngx-spinner';
+
+@NgModule({
+  exports: [NgxSpinnerService],
+  imports: [NgxSpinnerService],
+})
+export class SharedModule {}
