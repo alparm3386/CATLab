0982b4937e53ad1cfba92abd7638fe5b65eba1b2
Alpar Meszaros <alpar.meszaros@toppandigital.com>
2023-09-30 06:28:49 +0200

    spinner for monitoring.

--- CAT-main/WebUI/Backoffice/angular/projects/monitoring/src/app/app.component.ts
+++ CAT-main/WebUI/Backoffice/angular/projects/monitoring/src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MonitoringComponent } from './components/monitoring/monitoring.component';
 import { DataService } from './services/data.service';
+import { SpinnerService } from '../../../cat-common/services/spinner.service';
 
 @Component({
   selector: 'app-root',
@@ -14,11 +15,20 @@ import { DataService } from './services/data.service';
 })
 export class AppComponent implements OnInit {
   data: any;
+  title = 'Monitoring';
 
-  constructor(private dataService: DataService) { }
+  constructor(private dataService: DataService, private spinnerService: SpinnerService) { }
 
   ngOnInit(): void {
+    //the spinner
+    this.dataService.data$.subscribe(data => {
+      this.spinnerService.hide();
+      if (data) {
+      }
+    });
+    this.spinnerService.show();
+
+    //get the data from the server
     this.dataService.fetchData();
   }
-  title = 'Monitoring';
 }
