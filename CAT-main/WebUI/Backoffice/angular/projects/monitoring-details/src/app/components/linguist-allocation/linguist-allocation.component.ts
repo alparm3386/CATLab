e14dfc4a9b77e38910b946cc6a782f5ff4b4a6a0
Alpar Meszaros <alpar.meszaros@toppandigital.com>
2023-10-01 10:27:25 +0200

    linguist search refactoring.

--- CAT-main/WebUI/Backoffice/angular/projects/monitoring-details/src/app/components/linguist-allocation/linguist-allocation.component.ts
+++ CAT-main/WebUI/Backoffice/angular/projects/monitoring-details/src/app/components/linguist-allocation/linguist-allocation.component.ts
@@ -1,13 +1,15 @@
 import { Component, Input } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
-import { LinguistSearchComponent, LinguistSearchParam } from '../linguist-search/linguist-search.component';
 import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
+import { HttpClient } from '@angular/common/http';
+import { ModalService } from '../../../../../cat-common/services/modal.service';
 
 
 @Component({
   selector: 'app-linguist-allocation',
   standalone: true,
-  imports: [CommonModule, LinguistSearchComponent],
+  imports: [CommonModule, FormsModule],
   templateUrl: './linguist-allocation.component.html',
   styleUrls: ['./linguist-allocation.component.scss']
 })
@@ -15,19 +17,49 @@ export class LinguistAllocationComponent {
   @Input() jobData: any;
   @Input() allocation: any;
 
-  public linguistSearchParam?: LinguistSearchParam;
+  public searchTerm: string = '';
+  public linguists: any[] = [];
+  public throttledSearch: any;
+  public isLoading: boolean = false;
 
-  constructor(public activeModal: NgbActiveModal) {
+  constructor(private http: HttpClient, public activeModal: NgbActiveModal, private modalService: ModalService) {
   }
 
   ngOnInit(): void {
-    this.linguistSearchParam = {
-      sourceLang: this.jobData.sourceLanguage,
-      targetLang: this.jobData.targetLanguage,
-      speciality: this.jobData.speciality,
-      task: this.allocation.task
-    };
+    this.getLinguists();
   }
+
+  searchLinguists() {
+    if (this.searchTerm.trim().length < 3) {
+      this.linguists = [];
+      return;
+    }
+  }
+
+  getLinguists() {
+    // Get the filtered list of linguists from the server
+    this.isLoading = true;
+    this.http.get<any[]>('/api/Common/GetFilteredLinguists', {
+      params: {
+        sourceLanguageId: this.jobData.sourceLanguage,
+        targetLanguageId: this.jobData.targetLanguage,
+        speciality: this.jobData.speciality,
+        task: this.allocation.task
+      }
+    }).subscribe({
+      next: data => {
+        this.isLoading = false;
+        this.linguists = data;
+      },
+      error: error => {
+        this.isLoading = false;
+        this.modalService.alert("Unable to retrieve linguists from the server. Please try again later.", "Error");
+        //this.dataSubject.error(error);
+      }
+    });
+  }
+
+
   close(): void {
     this.activeModal.close();
   }
