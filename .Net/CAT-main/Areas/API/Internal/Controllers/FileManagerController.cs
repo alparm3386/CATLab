265207413898823889cfae43f7d213ef62ca1421
Alpar Meszaros <alpar.meszaros@toppandigital.com>
2023-10-25 21:13:47 +0200

    -

--- .Net/CAT-main/Areas/API/Internal/Controllers/FileManagerController.cs
+++ .Net/CAT-main/Areas/API/Internal/Controllers/FileManagerController.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System.Security.Claims;
+using static Lucene.Net.Util.Fst.Util;
 using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace CAT.Areas.API.Internal.Controllers
@@ -14,80 +15,110 @@ namespace CAT.Areas.API.Internal.Controllers
     [ApiController]
     public class FileManagerController : ControllerBase
     {
-    }
 
-    [HttpGet("list")]
-    public async Task<IActionResult> ListFilesAndDirectories(string directoryPath)
-    {
-        try
+        [HttpGet("list")]
+        public IActionResult ListFilesAndDirectories(string directoryPath)
         {
-            // Implement logic to list files and directories in the specified directoryPath.
-            var directoryInfo = new DirectoryInfo(directoryPath);
-
-            if (!directoryInfo.Exists)
+            try
             {
-                return NotFound($"Directory not found: {directoryPath}");
+                // Implement logic to list files and directories in the specified directoryPath.
+                var directoryInfo = new DirectoryInfo(directoryPath);
+
+                if (!directoryInfo.Exists)
+                {
+                    return NotFound($"Directory not found: {directoryPath}");
+                }
+
+                var files = directoryInfo.GetFiles().Select(file => new
+                {
+                    Name = file.Name,
+                    SizeInBytes = file.Length,
+                    LastModified = file.LastWriteTimeUtc
+                });
+
+                var directories = directoryInfo.GetDirectories().Select(directory => new
+                {
+                    Name = directory.Name,
+                    LastModified = directory.LastWriteTimeUtc
+                });
+
+                var result = new
+                {
+                    Files = files,
+                    Directories = directories
+                };
+
+                return Ok(result);
             }
-
-            var files = directoryInfo.GetFiles().Select(file => new
+            catch (Exception ex)
             {
-                Name = file.Name,
-                SizeInBytes = file.Length,
-                LastModified = file.LastWriteTimeUtc
-            });
-
-            var directories = directoryInfo.GetDirectories().Select(directory => new
-            {
-                Name = directory.Name,
-                LastModified = directory.LastWriteTimeUtc
-            });
-
-            var result = new
-            {
-                Files = files,
-                Directories = directories
-            };
-
-            return Ok(result);
+                return StatusCode(500, $"An error occurred: {ex.Message}");
+            }
         }
-        catch (Exception ex)
+
+        [HttpPost("create-directory")]
+        public IActionResult CreateDirectory(string directoryPath)
         {
-            return StatusCode(500, $"An error occurred: {ex.Message}");
+            try
+            {
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, $"An error occurred: {ex.Message}");
+            }
         }
-    }
 
-    [HttpPost("create-directory")]
-    public IActionResult CreateDirectory(string directoryPath)
-    {
-        // Implement logic to create a new directory at the specified directoryPath.
-        // Return a success or error response.
-    }
+        [HttpDelete("delete-directory")]
+        public IActionResult DeleteDirectory(string directoryPath)
+        {
+            try
+            {
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, $"An error occurred: {ex.Message}");
+            }
+        }
 
-    [HttpDelete("delete-directory")]
-    public IActionResult DeleteDirectory(string directoryPath)
-    {
-        // Implement logic to delete the directory at the specified directoryPath.
-        // Return a success or error response.
-    }
+        [HttpPut("rename-directory")]
+        public IActionResult RenameDirectory(string oldPath, string newPath)
+        {
+            try
+            {
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, $"An error occurred: {ex.Message}");
+            }
+        }
 
-    [HttpPut("rename-directory")]
-    public IActionResult RenameDirectory(string oldPath, string newPath)
-    {
-        // Implement logic to rename the directory from oldPath to newPath.
-        // Return a success or error response.
-    }
+        [HttpPost("upload-file")]
+        public async Task<IActionResult> UploadFile([FromForm] IFormFile file, string directoryPath)
+        {
+            try
+            {
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, $"An error occurred: {ex.Message}");
+            }
+        }
 
-    [HttpPost("upload-file")]
-    public async Task<IActionResult> UploadFile([FromForm] IFormFile file, string directoryPath)
-    {
-        // Implement logic to handle file upload and save the file to the specified directoryPath.
-        // Return a success or error response.
-    }
-
-    [HttpPut("rename-file")]
-    public IActionResult RenameFile(string oldPath, string newPath)
-    {
-        // Implement logic to rename the file from oldPath to newPath.
-        // Return a success or error response.
+        [HttpPut("rename-file")]
+        public IActionResult RenameFile(string oldPath, string newPath)
+        {
+            try
+            {
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, $"An error occurred: {ex.Message}");
+            }
+        }
     }
 }
