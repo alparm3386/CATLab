861469cbc1561ed10e9358f29fd3604b435bc36c
Alpar Meszaros <alpar.meszaros@toppandigital.com>
2023-10-27 00:49:27 +0200

    -

--- .Net/TestApp/OkapiConnector.cs
+++ .Net/TestApp/OkapiConnector.cs
@@ -34,7 +34,7 @@ namespace TestApp
 
         private EndpointAddress GetOkapiServiceEndpoint()
         {
-            var endPointAddr = "http://localhost:8080/services/OkapiService";
+            var endPointAddr = "http://159.223.246.57:8080/services/OkapiService";
 
             //create the endpoint address for the 
             return new EndpointAddress(endPointAddr);
