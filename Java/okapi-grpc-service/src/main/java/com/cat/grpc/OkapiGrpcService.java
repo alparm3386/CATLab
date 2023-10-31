e91ac5a018871a5965665f9d8adaacd82d2c277c
Alpar Meszaros <alpar.meszaros@toppandigital.com>
2023-10-31 08:31:26 +0100

    -

--- Java/okapi-grpc-service/src/main/java/com/cat/grpc/OkapiGrpcService.java
+++ Java/okapi-grpc-service/src/main/java/com/cat/grpc/OkapiGrpcService.java
@@ -1,16 +1,36 @@
 package com.cat.grpc;
 
 import io.grpc.stub.StreamObserver;
+
+import static io.grpc.stub.ServerCalls.asyncUnimplementedUnaryCall;
+
 import com.cat.grpc.OkapiGrpc.OkapiImplBase;
 import com.cat.grpc.OkapiService.*;
+import com.google.protobuf.ByteString;
 
 public class OkapiGrpcService extends OkapiImplBase {
-    @Override
-    public void sayHello(HelloRequest request, StreamObserver<HelloResponse> responseObserver) {
-        String greeting = "Hello, " + request.getName() + "!";
-        
-        HelloResponse response = HelloResponse.newBuilder().setMessage(greeting).build();
-        responseObserver.onNext(response);
-        responseObserver.onCompleted();
-    }
+	@Override
+	public void sayHello(HelloRequest request, StreamObserver<HelloResponse> responseObserver) {
+		String greeting = "Hello, " + request.getName() + "!";
+
+		HelloResponse response = HelloResponse.newBuilder().setMessage(greeting).build();
+		responseObserver.onNext(response);
+		responseObserver.onCompleted();
+	}
+
+	@Override
+	public void createDocumentFromXliff(com.cat.grpc.OkapiService.CreateDocumentFromXliffRequest request,
+			io.grpc.stub.StreamObserver<com.cat.grpc.OkapiService.CreateDocumentFromXliffResponse> responseObserver) {
+		var response = CreateDocumentFromXliffResponse.newBuilder().setCreatedDocument(ByteString.copyFrom(new byte[] {10, 20})).build();
+		responseObserver.onNext(response);
+		responseObserver.onCompleted();
+	}
+
+	@Override
+	public void createXliffFromDocument(com.cat.grpc.OkapiService.CreateXliffFromDocumentRequest request,
+			io.grpc.stub.StreamObserver<com.cat.grpc.OkapiService.CreateXliffFromDocumentResponse> responseObserver) {
+		var response = CreateXliffFromDocumentResponse.newBuilder().setXliffContent("xliff content comes here.").build();
+		responseObserver.onNext(response);
+		responseObserver.onCompleted();
+	}
 }
