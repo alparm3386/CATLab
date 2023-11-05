90d9ffb365378b04ec8995cd98587c04bbca24bc
Alpar Meszaros <alpar.meszaros@toppandigital.com>
2023-11-05 21:27:53 +0100

    -

--- .Net/CAT-main/WebUI/Backoffice/angular/projects/monitoring/src/app/components/monitoring/order/job/workflow/workflow.component.ts
+++ .Net/CAT-main/WebUI/Backoffice/angular/projects/monitoring/src/app/components/monitoring/order/job/workflow/workflow.component.ts
@@ -9,5 +9,18 @@ import { CommonModule } from '@angular/common';
   styleUrls: ['./workflow.component.scss']
 })
 export class WorkflowComponent {
-  @Input() workflowSteps: any; // Declare an input property
+  private _workflowSteps: any; // Private variable to store the input data
+
+  @Input()
+  set workflowSteps(steps: any) {
+    // Sort the input array when it is set
+    this._workflowSteps = steps.sort((a: any, b: any) => {
+      return a.stepOrder - b.stepOrder;
+    });
+  }
+
+  get workflowSteps(): any[] {
+    return this._workflowSteps;
+  }
+
 }
