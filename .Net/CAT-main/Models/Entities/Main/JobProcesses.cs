79f31dabf2c0e2352b2ed32a83fcb904e3611062
Alpar Meszaros <alpar.meszaros@toppandigital.com>
2023-11-16 12:01:45 +0100

    -

--- .Net/CAT-main/Models/Entities/Main/JobProcesses.cs
+++ .Net/CAT-main/Models/Entities/Main/JobProcesses.cs
@@ -3,28 +3,22 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace CAT.Models.Entities.Main
 {
-    [Table("Jobs")]
-    public class Job
+    [Table("JobProcesses")]
+    public class JobProcess
     {
         [Key]
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
 
-        public int OrderId { get; set; }
+        public int JobId { get; set; }
 
         [System.Diagnostics.CodeAnalysis.MaybeNull]
-        public virtual Order Order { get; set; }
-
-        public int QuoteId { get; set; }
+        public DateTime ProcessId { get; set; }
 
         [System.Diagnostics.CodeAnalysis.MaybeNull]
-        public virtual Quote Quote { get; set; }
-
-        public int SourceDocumentId { get; set; }
-
-        public int FinalDocumentId { get; set; }
+        public DateTime ProcessStarted { get; set; }
 
         [System.Diagnostics.CodeAnalysis.MaybeNull]
-        public ICollection<WorkflowStep> WorkflowSteps { get; set; }
+        public DateTime ProcessEnded { get; set; }
     }
 }
