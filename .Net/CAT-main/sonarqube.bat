583d1826f8322841ebbeca88000825c1ff294bb1
alparm <alpar.meszaros@gmail.com>
2024-01-15 21:14:34 +0100

    -

--- .Net/CAT-main/sonarqube.bat
+++ .Net/CAT-main/sonarqube.bat
@@ -1,10 +1,10 @@
 REM unit test
-cd ../CAT-main.UnitTests
+cd ../UnitTests/CAT-main.UnitTests
 dotnet test /p:CollectCoverage=true /p:CoverletOutputFormat=opencover /p:CoverletOutput=./TestResults/
 REM sonarscanner
 cd ../CAT-main
-dotnet sonarscanner begin /k:"CAT-main" /d:sonar.host.url="http://localhost:9000"  /d:sonar.token="sqp_26299e0970cb343f2a81f1d10bbdeb6305cfe60d"
+dotnet sonarscanner begin /k:"Cat-main" /d:sonar.host.url="http://localhost:9000"  /d:sonar.token="sqp_e9d7a1cc7cf38e35cb3308775dc695b38eac8d03"
 dotnet build 
-dotnet sonarscanner end /d:sonar.token="sqp_26299e0970cb343f2a81f1d10bbdeb6305cfe60d"
+dotnet sonarscanner end /d:sonar.token="sqp_e9d7a1cc7cf38e35cb3308775dc695b38eac8d03"
 REM pause
 pause
